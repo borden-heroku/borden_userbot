09c2399d1a9748040aa3d9d8456a29aaf07814a5
Princic=1837592 <andreapri.99@gmail.com>
2020-09-14 12:32:42 +0200

    first commit, luke03 and borden sessions

---
+++ main.py
@@ -0,0 +1,53 @@
+from typing import List
+import asyncio
+from pyrogram import Client,idle
+from pyrogram.handlers import MessageHandler
+from pyrogram import filters
+from configparser import ConfigParser
+from pyrogram.errors import UserNotParticipant
+from pyrogram.types import Message,User
+from random import shuffle
+
+
+async def invite_people(client: Client,message: Message):
+    from_group,to_group,max_number=message.command[1].strip('@'),\
+                               message.command[2].strip('@'),\
+                               min(50,int(message.command[3]))
+    client_id=(await client.get_me()).id
+    try:
+        try:
+            await client.get_chat_member(from_group,client_id)
+        except UserNotParticipant:
+            await client.join_chat(from_group)
+        added=0
+        members=await client.get_chat_members(from_group,200)
+        shuffle(members)
+        for member in members:
+            user: User=member.user
+            if user.is_bot:
+                continue
+            try:
+                await client.add_chat_members(to_group,user.id)
+            except Exception as e:
+                print(e)
+                continue
+            added+=1
+            if added>=max_number:
+                break
+        await message.reply_text(f'Added {added} members from @{from_group} to @{to_group}')
+    except Exception as e:
+        await message.reply_text(str(e))
+
+
+async def main(sessions: List[str]):
+    clients: List[Client]=list()
+    for session in sessions:
+        clients.append(client:=Client(session,phone_number=session))
+        client.add_handler(MessageHandler(invite_people,filters.command('add')))
+        await client.start()
+    await idle()
+
+
+config=ConfigParser()
+config.read('config.ini')
+asyncio.get_event_loop().run_until_complete(main(config['adder']['sessions'].split()))
---
+++ test.py
@@ -0,0 +1,14 @@
+from pyrogram import Client
+import asyncio
+
+
+async def main():
+    client=Client('+2347054467363')
+    await client.start()
+    async for dialog in client.iter_dialogs():
+        chat=dialog.chat
+        print(chat.type,chat.username if chat.username else chat.title,chat.id)
+    await client.stop()
+
+
+asyncio.get_event_loop().run_until_complete(main())
